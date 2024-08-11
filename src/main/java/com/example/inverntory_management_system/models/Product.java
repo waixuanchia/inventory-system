90a5a7196e586f3d033daf5de1c6a58830611b0a
Chia Wai Xuan <waixuanchia@gmail.com>
2024-08-11 07:55:59 -0700

    add product model

---
+++ src/main/java/com/example/inverntory_management_system/models/Product.java
@@ -0,0 +1,57 @@
+package com.example.inverntory_management_system.models;
+
+import jakarta.persistence.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.util.Date;
+
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="products")
+public class Product {
+  @Id
+  @GeneratedValue(strategy= GenerationType.IDENTITY)
+  private Long id;
+
+  private String name;
+
+  @OneToOne(cascade = CascadeType.ALL)
+  @JoinColumn(name="supplier_id",referencedColumnName="id")
+  private Supplier supplier;
+
+  @OneToOne(cascade=CascadeType.ALL)
+  @JoinColumn(name="unit_id",referencedColumnName="id")
+  private Unit unit;
+
+  @OneToOne(cascade=CascadeType.ALL)
+  @JoinColumn(name="category_id",referencedColumnName="id")
+  private Category category;
+
+  private int quantity;
+
+  @Column(name="status",nullable = true,columnDefinition="TINYINT Default 1")
+  private Byte status;
+
+  public Long createdBy;
+
+  public Long updatedBy;
+
+  @CreationTimestamp
+  public Date creationDate;
+
+  @UpdateTimestamp
+  public Date updatedDate;
+
+
+}
