4b767798997322eec0f4e0aca266e0866328fb1c
Chia Wai Xuan <waixuanchia@gmail.com>
2024-08-20 07:22:31 -0700

    edit product

--- src/main/java/com/example/inverntory_management_system/controller/ProductController.java
+++ src/main/java/com/example/inverntory_management_system/controller/ProductController.java
@@ -3,6 +3,7 @@ package com.example.inverntory_management_system.controller;
 
 import com.example.inverntory_management_system.dto.ProductDto;
 import com.example.inverntory_management_system.mapper.ProductMapper;
+import com.example.inverntory_management_system.mapper.UnitMapper;
 import com.example.inverntory_management_system.models.Category;
 import com.example.inverntory_management_system.models.Product;
 import com.example.inverntory_management_system.models.Supplier;
@@ -16,13 +17,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/product")
@@ -66,6 +65,7 @@ public class ProductController {
                             Model model,
                             RedirectAttributes redirectAttributes){
     if(bindingResult.hasErrors()){
+
       model.addAttribute("alertType","error");
       model.addAttribute("alertMessage","There are errors within the form");
       List<Supplier> suppliers = this.supplierService.listSupplier();
@@ -86,15 +86,55 @@ public class ProductController {
   }
 
   @GetMapping("/edit-product/{productId}")
-  public String editProduct(Model model){
-    ProductDto product = new ProductDto();
-    List<Supplier> suppliers = this.supplierService.listSupplier();
-    List<Category> categories = this.categoryService.listCategories();
-    List<Unit> units = this.unitService.listUnits();
-    model.addAttribute("product",product);
-    model.addAttribute("suppliers",suppliers);
-    model.addAttribute("categories",categories);
-    model.addAttribute("units",units);
-    return "product/addProduct";
+  public String editProduct(Model model,
+                            @PathVariable("productId") Long productId,
+                            RedirectAttributes redirectAttributes){
+    Optional<Product> productOptional = this.productService.findById(productId);
+    if(productOptional.isPresent()){
+      Product product = productOptional.get();
+      List<Supplier> suppliers = this.supplierService.listSupplier();
+      List<Category> categories = this.categoryService.listCategories();
+      List<Unit> units = this.unitService.listUnits();
+      model.addAttribute("product",product);
+      model.addAttribute("suppliers",suppliers);
+      model.addAttribute("categories",categories);
+      model.addAttribute("units",units);
+      return "product/editProduct";
+    }
+    else{
+      redirectAttributes.addFlashAttribute("alertType","error");
+      redirectAttributes.addFlashAttribute("alertMessage","Unable to find product");
+      return "redirect:/product";
+    }
+
+  }
+
+  @PostMapping("/edit-product/{productId}")
+  public String saveEditedProduct(Model model,
+                            @ModelAttribute("product") ProductDto productDto,
+                            BindingResult bindingResult,
+                            @PathVariable("productId") Long productId,
+                            RedirectAttributes redirectAttributes){
+    if(bindingResult.hasErrors()){
+      model.addAttribute("alertType","error");
+      model.addAttribute("alertMessage","There are errors in the form");
+      model.addAttribute("product",productDto);
+      List<Supplier> suppliers = this.supplierService.listSupplier();
+      List<Category> categories = this.categoryService.listCategories();
+      List<Unit> units = this.unitService.listUnits();
+
+      model.addAttribute("suppliers",suppliers);
+      model.addAttribute("categories",categories);
+      model.addAttribute("units",units);
+      return "unit/editUnit";
+    }
+    else{
+      Product product = ProductMapper.mapToProduct(productDto);
+      this.productService.saveProduct(product);
+      redirectAttributes.addFlashAttribute("alertType","success");
+      redirectAttributes.addFlashAttribute("alertMessage","product save success");
+      return "redirect:/product/edit-product/" + productId;
+    }
+
   }
 }
